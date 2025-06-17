cfa88fc0934ee1ce791fd4e6267bebcea44db42d
adrija2205 <adrijabhowmickslg@gmail.com>
2025-06-17 16:39:21 +0530

    Create set_matrixzero.java
    set_matrixzero question solution

---
+++ Arrays/set_matrixzero.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        List<int[]> zeroes = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    zeroes.add(new int[]{i, j});
+                }
+            }
+        }
+        for (int[] pos : zeroes) {
+            int row = pos[0], col = pos[1];
+            for (int i = 0; i < m; i++) {
+                matrix[i][col] = 0;
+            }
+            for (int j = 0; j < n; j++) {
+                matrix[row][j] = 0;
+            }
+        }
+    }
+}
